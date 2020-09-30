f1513520bd04fe36f64097c28e18bff93777517d
Rodrigo <rodrigo.garcia@estudiantes.matcom.uh.cu>
2020-09-30 16:22:28 -0400

    Add tests

---
+++ tests/get_ast/test_get_ast_1.py
@@ -0,0 +1,26 @@
+# from lark import Tree
+# from src.parsing import parser
+# from src.get_ast import GetTree
+
+
+def test1():
+    #     test1 = """
+    #         class Main inherits Object {
+    #             a : Int <- asd ;
+    #             b : String <- 1000;
+    #             c : Bool <- true;
+
+    #             met ( d : Int ) : String {
+    #                 c <- false
+    #             } ;
+    #             } ;
+    #             """
+
+    #     res_tree_1 = parser.parse(test1)
+
+    #     Ast = GetTree().transform(res_tree_1)
+
+    #     print(Ast)
+
+    assert True
+
